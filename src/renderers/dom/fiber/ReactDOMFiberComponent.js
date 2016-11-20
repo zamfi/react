a25bf56a56f450500c906fbb8af63f7f9a8b1a43
Nathan Hunzaker <nate.hunzaker@gmail.com>
2016-11-20 16:47:47 -0500

    Migrate over rules for DOM Fiber. Update tests

--- src/renderers/dom/fiber/ReactDOMFiberComponent.js
+++ src/renderers/dom/fiber/ReactDOMFiberComponent.js
@@ -14,7 +14,6 @@
 
 var CSSPropertyOperations = require('CSSPropertyOperations');
 var DOMNamespaces = require('DOMNamespaces');
-var DOMProperty = require('DOMProperty');
 var DOMPropertyOperations = require('DOMPropertyOperations');
 var EventPluginRegistry = require('EventPluginRegistry');
 var ReactBrowserEventEmitter = require('ReactBrowserEventEmitter');
@@ -211,10 +210,7 @@ function setInitialDOMProperties(
       }
     } else if (isCustomComponentTag) {
       DOMPropertyOperations.setValueForAttribute(domElement, propKey, nextProp);
-    } else if (
-      DOMProperty.properties[propKey] ||
-      DOMProperty.isCustomAttribute(propKey)
-    ) {
+    } else {
       // If we're updating to null or undefined, we should remove the property
       // from the DOM node instead of inadvertently setting to a string. This
       // brings us in line with the same behavior we have on initial render.
@@ -256,10 +252,7 @@ function updateDOMProperties(
       } else {
         DOMPropertyOperations.deleteValueForAttribute(domElement, propKey);
       }
-    } else if (
-      DOMProperty.properties[propKey] ||
-      DOMProperty.isCustomAttribute(propKey)
-    ) {
+    } else {
       // If we're updating to null or undefined, we should remove the property
       // from the DOM node instead of inadvertently setting to a string. This
       // brings us in line with the same behavior we have on initial render.
@@ -343,7 +336,7 @@ var ReactDOMFiberComponent = {
       } else if (props.is) {
         domElement = ownerDocument.createElement(type, {is: props.is});
       } else {
-        // Separate else branch instead of using `props.is || undefined` above because of a Firefox bug.
+        // Separate else branch instead of using `props.is || undefined` above becuase of a Firefox bug.
         // See discussion in https://github.com/facebook/react/pull/6896
         // and discussion in https://bugzilla.mozilla.org/show_bug.cgi?id=1276240
         domElement = ownerDocument.createElement(type);
@@ -400,7 +393,6 @@ var ReactDOMFiberComponent = {
       case 'object':
       case 'source':
       case 'video':
-      case 'details':
         trapBubbledEventsLocal(domElement, tag);
         props = rawProps;
         break;
